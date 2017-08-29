e3fed6e0cf1283b206092d146fe99cebb14c43f0
user <user@example.com>
2017-08-29 20:10:13 +0900

    Initial commit

---
+++ build.fsx
@@ -0,0 +1,52 @@
+// include Fake libs
+#r "./packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+// Directories
+let buildDir  = "./build/"
+let deployDir = "./deploy/"
+
+
+// Filesets
+let appReferences  =
+    !! "/**/*.csproj"
+    ++ "/**/*.fsproj"
+
+// Application info
+let appName = "MidiToWavConverter"
+let version = "0.1"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; deployDir]
+)
+
+Target "Build" (fun _ ->
+    // compile all projects below src/app/
+    MSBuildDebug buildDir "Build" appReferences
+    |> Log "AppBuild-Output: "
+)
+
+Target "Deploy" (fun _ ->
+    !! (buildDir + "/**/*.*")
+    -- "*.zip"
+    |> Zip buildDir (deployDir + appName + "." + version + ".zip")
+)
+
+Target "Run" (fun _ ->
+    let exeFileName = buildDir + appName + ".exe"
+    let result = ExecProcess (fun info -> info.FileName <- exeFileName) System.TimeSpan.MaxValue
+    if result <> 0 then
+        failwithf "Process abend with code %d" result)
+
+// Build order
+"Clean"
+    ==> "Build"
+    ==> "Deploy"
+
+"Build"
+    ==> "Run"
+
+// start build
+RunTargetOrDefault "Run"
